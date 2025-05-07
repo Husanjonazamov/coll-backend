3bb6e2cb128b805a3c8ce4f3cf2c8c08a58dec86
Husanjonazamov <azamovhusanboy08@gmail.com>
2025-05-07 19:16:41 +0500

    Qo'shildi: user router

--- core/apps/bot/urls.py
+++ core/apps/bot/urls.py
@@ -2,12 +2,15 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from core.apps.bot.views import (
     CategoryView,
-    TeacherView
+    TeacherView, UserView
 )
 
 router = DefaultRouter()
 router.register(r"category", CategoryView, basename="category")
 router.register(r"teacher", TeacherView, basename="teacher")
+
+# users
+router.register(r"users", UserView, basename="user")
 
 urlpatterns = [
     path("", include(router.urls)),
