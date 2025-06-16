a4fb0e54f1cefd72c0142e18d1f9ea1af501ba26
Husanjonazamov <azamovhusanboy08@gmail.com>
2025-06-16 14:25:05 +0500

    Request blocked, get in touch, we can come up with a solution for your use case.

--- core/apps/bot/views/teacher.py
+++ core/apps/bot/views/teacher.py
@@ -6,11 +6,44 @@ from rest_framework.viewsets import ReadOnlyModelViewSet
 from core.apps.bot.models import TeacherModel
 from core.apps.bot.serializers.teacher import CreateTeacherSerializer, ListTeacherSerializer, RetrieveTeacherSerializer
 
+
+from rest_framework.pagination import PageNumberPagination
+from rest_framework.response import Response
+import math
+
+
+class CustomPagination(PageNumberPagination):
+    page_size = 100 
+    page_size_query_param = 'page_size'
+    max_page_size = 100
+
+    def get_paginated_response(self, data):
+        return Response({
+            "status": True,
+            "data": {
+                "links": {
+                    "previous": self.get_previous_link(),
+                    "next": self.get_next_link()
+                },
+                "total_items": self.page.paginator.count,
+                "total_pages": math.ceil(self.page.paginator.count / self.page_size),
+                "page_size": self.page_size,
+                "current_page": self.page.number,
+                "results": data
+            }
+        })
+
+
+
+
+
+
 @extend_schema(tags=["teacher"])
 class TeacherView(BaseViewSetMixin, ReadOnlyModelViewSet):
     queryset = TeacherModel.objects.all()
     serializer_class = ListTeacherSerializer
     permission_classes = [AllowAny]
+    pagination_class = CustomPagination  
 
     action_permission_classes = {}
     action_serializer_class = {
